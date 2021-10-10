44da0d3620396e38cae4598a17da9f793d9fdb72
logan-lieou <loganj@fedora.xyz>
2021-10-10 16:40:58 -0500

    model

--- ALZHEIMERS/src/main.py
+++ ALZHEIMERS/src/main.py
@@ -1,6 +1,7 @@
 # deps
-import matplotlib.image as mpimg
 import os
+import matplotlib.image as mpimg
+import numpy as np
 
 # example dir
 CUR_DIR = "DATA/train/MildDemented/"
@@ -10,3 +11,8 @@ def gen_case(direc):
     ims_list = os.listdir(direc)
     ims_tensor = [mpimg.imread(direc + x) for x in ims_list]
     return ims_tensor
+
+# shape of one img is (208, 176)
+imgs = gen_case(CUR_DIR)
+print(imgs[0].shape) # 208, 176
+print(np.array(imgs).shape) # large tensor shape 717, 208, 176
