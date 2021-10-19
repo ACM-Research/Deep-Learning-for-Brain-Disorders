f0af6de71721b5499d683c79bb8228778291e88d
logan-lieou <loganj@fedora.xyz>
2021-10-18 21:43:15 -0500

    data process mvp

--- ALZHEIMERS/src/main.py
+++ ALZHEIMERS/src/main.py
@@ -1,18 +1,24 @@
 # deps
 import os
+import pandas as pd
 import matplotlib.image as mpimg
 import numpy as np
 
-# example dir
-CUR_DIR = "DATA/train/MildDemented/"
+direc = "./DATA/train/"
+dirs_list = os.listdir(direc)
 
-# read in an entire folder of files
-def gen_case(direc):
-    ims_list = os.listdir(direc)
-    ims_tensor = [mpimg.imread(direc + x) for x in ims_list]
-    return ims_tensor
+images = []
+labels = []
+for i in range(len(dirs_list)):
+    y = [mpimg.imread(direc + dirs_list[i] + "/" + x) for x in os.listdir(direc + dirs_list[i])]
+    t = np.tile(i, len(y))
+    images.append(y)
+    labels.append(t)
 
-# shape of one img is (208, 176)
-imgs = gen_case(CUR_DIR)
-print(imgs[0].shape) # 208, 176
-print(np.array(imgs).shape) # large tensor shape 717, 208, 176
+images = pd.Series(images, name="Images")
+labels = pd.Series(labels, name="Labels")
+
+df = pd.concat([images, labels], axis=1, join='outer')
+df = df.sample(frac=1)
+
+df.to_csv("train.csv")
