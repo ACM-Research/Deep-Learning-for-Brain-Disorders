f1d210b7616b9285a868441a159487ed87c24c84
LoganLieou <logan.lieou@gmail.com>
2021-11-03 19:04:56 -0500

    more house keeping

--- AUTISM/DEV/DATA_PROCESSING/Vis.R
+++
@@ -1,3 +0,0 @@
-# https://rdrr.io/rforge/oro.nifti/f/inst/doc/nifti.pdf
-temp_brain <- readNIfTI("DATA/50054/MP-RAGE/2000-01-01_00_00_00.0/I328862/ABIDE_50054_MRI_MP-RAGE_br_raw_20120830185721042_S164852_I328862.nii")
-oro.nifti::image(temp_brain)
